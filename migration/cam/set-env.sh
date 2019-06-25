8548ecc27fef42eb8a465178c8b966befcfa4d0e
Todd Sanders <tsanders@redhat.com>
2019-06-25 12:32:02 -0400

    update

--- migration/cam/set-env.sh
+++ migration/cam/set-env.sh
@@ -30,7 +30,7 @@ HOSTAPI='https://master:8443' ./deploy.sh
 
 #Update CORS config
 dahostname=$(oc get routes -n mig | grep ^mig-ui | awk '{print $2}' | sed 's/\./\\\\./g')
-awk 'FNR==NR{ if (/(?i)/) p=NR; next} 1; FNR==p{ print "- (?i)//'"$dahostname"'(:|\\z)" }' /etc/origin/master/master-config.yaml /etc/origin/master/master-config.yaml > tmpfile && mv tmpfile /etc/origin/master/master-config.yaml
+awk 'FNR==NR{ if (/(?i)/) p=NR; next} 1; FNR==p{ print "- (?i)//'"$dahostname"'(:|\\z)" }' /etc/origin/master/master-config.yaml /etc/origin/master/master-config.yaml > tmpfile && mv -f tmpfile /etc/origin/master/master-config.yaml
 
 #Restart Master
 master-restart api
